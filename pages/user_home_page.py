c12176e90763040eef5c4ff65b73390e111505ac
Mannyyo <manoel.lammi@gmail.com>
2025-11-19 12:39:32 -0300

    Atualizando UserHomePage com métodos semânticos

--- pages/user_home_page.py
+++ pages/user_home_page.py
@@ -12,16 +12,16 @@ class UserHomePage(BasePage):
     # -----------------------------------
 
     MENU_PROTOCOLOS = (By.CSS_SELECTOR, "#conteudo > div.cad_conteudo > div:nth-child(5)")
-    MENU_A_RECEBER = (By.CSS_SELECTOR, "#mostrarProtocolosAReceber")
+    MENU_PROTOCOLO_A_RECEBER = (By.CSS_SELECTOR, "#mostrarProtocolosAReceber")
 
-    SETOR_FILIAL_0 = (By.CSS_SELECTOR, "#mostrarProtocoloSetorFilial0")
-    SETOR_FILIAL_1 = (By.CSS_SELECTOR, "#mostrarProtocoloSetorFilial1")
-    SETOR_FILIAL_2 = (By.CSS_SELECTOR, "#mostrarProtocoloSetorFilial2")
+    CATEGORIA_0 = (By.CSS_SELECTOR, "#mostrarProtocoloSetorFilial0")
+    CATEGORIA_1 = (By.CSS_SELECTOR, "#mostrarProtocoloSetorFilial1")
+    CATEGORIA_2 = (By.CSS_SELECTOR, "#mostrarProtocoloSetorFilial2")
 
-    SETORES = {
-        0: SETOR_FILIAL_0,
-        1: SETOR_FILIAL_1,
-        2: SETOR_FILIAL_2,
+    CATEGORIAS = {
+        0: CATEGORIA_0,
+        1: CATEGORIA_1,
+        2: CATEGORIA_2,
     }
 
     # -----------------------------------
@@ -32,22 +32,33 @@ class UserHomePage(BasePage):
         self.click(self.MENU_PROTOCOLOS)
 
     def abrir_protocolos_a_receber(self):
-        self.click(self.MENU_A_RECEBER)
+        self.click(self.MENU_PROTOCOLO_A_RECEBER)
 
-    def abrir_setor_filial(self, numero: int):
-        if numero not in self.SETORES:
-            raise ValueError(f"Setor '{numero}' não é válido. Use 1, 2 ou 3.")
-        self.click(self.SETORES[numero])
+    def abrir_categoria(self, numero: int):
+        if numero not in self.CATEGORIAS:
+            raise ValueError(f"Setor '{numero}' não é válido. Use 0, 1 ou 2.")
+        self.click(self.CATEGORIAS[numero])
+
+    # -------- Métodos semânticos --------
+
+    def abrir_aguardando_resposta_despacho(self):
+        self.abrir_categoria(0)
+
+    def abrir_departamento_fiscalizacao(self):
+        self.abrir_categoria(1)
+
+    def abrir_pre_envio_para_camaras(self):
+        self.abrir_categoria(2)
 
     # -----------------------------------
     # Fluxo completo
     # -----------------------------------
 
-    def acessar_protocolos_setor(self, setor: int):
+    def acessar_categoria(self, categoria: int):
         """
         Fluxo completo:
-        Home → Protocolos → A Receber → Setor X
+        Home → Protocolos → A Receber → Categoria X
         """
         self.abrir_protocolos()
         self.abrir_protocolos_a_receber()
-        self.abrir_setor_filial(setor)
+        self.abrir_categoria(categoria)
